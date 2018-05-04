b41b5fa7968f08f63efd07d619c92f0074f7e4b6
Алексей Саванович <Sawanowi4.Alexey@yandex.by>
2018-05-04 12:50:44 +0300

    Add migration for create store procedure

--- OAA.Data/Migrations/ApplicationContextModelSnapshot.cs
+++ OAA.Data/Migrations/ApplicationContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace OAA.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.0-rtm-26452")
+                .HasAnnotation("ProductVersion", "2.0.2-rtm-10011")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("OAA.Data.Album", b =>
